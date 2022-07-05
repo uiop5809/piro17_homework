dd9d07cf94da82ad57ad2240c5c15b6c73575d11
parkyejin <uiop5809@naver.com>
2022-07-06 01:30:16 +0900

    Javascript 스톱워치 과제

---
+++ Javascript 스톱워치 과제/stopwatch.js
@@ -0,0 +1,78 @@
+const stopWatch = document.querySelector(".clock__time");
+const startEl = document.querySelector(".start");
+const stopEl = document.querySelector(".stop");
+const resetEl = document.querySelector(".reset");
+
+const recordBox = document.querySelector(".record__box");
+
+const allBtn = document.querySelector("#all");
+const delBtn = document.querySelector("#del");
+
+let time = 0;
+
+function startClock() {
+  time++;
+  sec = String(parseInt(time / 100)).padStart(2, "0");
+  msec = String(time % 100).padStart(2, "0");
+  stopWatch.textContent = `${sec}:${msec}`;
+  timer = setTimeout(startClock, 10);
+}
+
+function stopClock() {
+  clearTimeout(timer);
+  const li = document.createElement("li");
+  li.innerHTML = `
+    <i class="fa-regular fa-circle"></i>
+    <div>${stopWatch.textContent}</div>
+  `;
+  recordBox.appendChild(li);
+}
+
+function resetClock() {
+  time = 0;
+  clearTimeout(timer);
+  stopWatch.textContent = "00:00";
+}
+
+function allCheck() {
+  allBtn.classList.toggle("fa-circle-check");
+  let recordList = document.querySelectorAll(".record__box li i");
+  recordList.forEach((item) => {
+    if (allBtn.classList.contains("fa-circle-check")) {
+      item.classList.add("fa-circle-check");
+    } else {
+      item.classList.remove("fa-circle-check");
+    }
+  });
+}
+
+function delCheck() {
+  recordList = document.querySelectorAll(".record__box li i");
+  recordList.forEach((item) => {
+    if (item.classList.contains("fa-circle-check")) {
+      item.parentNode.remove();
+    }
+  });
+}
+
+function oneCheck() {
+  recordList = document.querySelectorAll(".record__box li i");
+  recordList.forEach((item) => {
+    item.addEventListener("click", () => {
+      if (item.classList.contains("fa-circle-check")) {
+        item.classList.remove("fa-circle-check");
+      } else {
+        item.classList.add("fa-circle-check");
+      }
+    });
+  });
+}
+
+startEl.addEventListener("click", startClock);
+stopEl.addEventListener("click", stopClock);
+resetEl.addEventListener("click", resetClock);
+
+allBtn.addEventListener("click", allCheck);
+delBtn.addEventListener("click", delCheck);
+
+recordBox.addEventListener("click", oneCheck);
